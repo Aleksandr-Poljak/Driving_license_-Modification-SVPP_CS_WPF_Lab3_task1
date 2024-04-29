102c495fba630369b37de33fcd0923bcf6e9dfdb
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2024-04-30 00:31:09 +0300

    init

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SVPP_CS_WPF_Lab3_task1_Driving_license__Modification_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
