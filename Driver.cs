e3473a419c4b460c18e52a5602736c45c941e9ef
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2024-05-03 15:17:56 +0300

    Added a set of neural network generated midjourney set of photos for the driver's license. Class changes. Added static method for creating an instance with a set of random data.

--- Driver.cs
+++ Driver.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xaml;
 using System.Xml.Linq;
 
 namespace SVPP_CS_WPF_Lab3_task1_Driving_license__Modification_
@@ -20,7 +23,7 @@ namespace SVPP_CS_WPF_Lab3_task1_Driving_license__Modification_
         private const int DEFAULT_NUMBER = 0;
         private const string DEFAULT_ADRESS = "Default Adress";
         private const string DEFAULT_PHOTO_PATH = "photo\\default_photo.jpg";
-        private const int DEFAULT_HGT = 119;
+        private const int DEFAULT_HGT = 120;
         private static readonly DateTime DEFAULT_DOB = new DateTime(1901, 1, 1);
         private static readonly DateTime DEFAULT_ISS = DEFAULT_DOB;
         private static readonly DateTime DEFAULT_EXP = DEFAULT_DOB;
@@ -54,6 +57,7 @@ namespace SVPP_CS_WPF_Lab3_task1_Driving_license__Modification_
             Gender = GenderEnum.other;
             Eyes = EyesEnum.Amber;
             HGT = DEFAULT_HGT;
+            Class_License = DEFAULT_CLASS_LICENSE;
 
         }
 
@@ -77,8 +81,11 @@ namespace SVPP_CS_WPF_Lab3_task1_Driving_license__Modification_
 
         public override string ToString()
         {
-            string dr = $"FIO: {NameSurname}\nLicense Number: {Number}\nLicense class {Class_License}\nAdress: {Adress}\nDOB: {DOB.ToShortDateString()}\nOrgan donor: {OrganDonor}\n" +
-                $"Photo: {PhotoPath}\n Gender: {Gender.ToString()}\nEyes: {Eyes.ToString()}\nHGT: {HGT}\n ISS: {ISS.ToShortDateString()}\nEXP: {EXP.ToShortDateString()}";
+            string dr = $"FIO: {NameSurname}\nLicense Number: {Number}\n" +
+                $"License class {Class_License}\nAdress: {Adress}\n" +
+                $"DOB: {DOB.ToShortDateString()}\nOrgan donor: {OrganDonor}\n" +
+                $"Photo: {PhotoPath}\n Gender: {Gender.ToString()}\nEyes: {Eyes.ToString()}\n" +
+                $"HGT: {HGT}\n ISS: {ISS.ToShortDateString()}\nEXP: {EXP.ToShortDateString()}";
             return dr;
         }
         /// <summary>
@@ -87,10 +94,75 @@ namespace SVPP_CS_WPF_Lab3_task1_Driving_license__Modification_
         public bool IsFullInfo()
         {
 
-            if (NameSurname == DEFAULT_NAME_SURNAME || Number == DEFAULT_NUMBER || Adress == DEFAULT_ADRESS || DOB == DEFAULT_DOB ||
-                PhotoPath == DEFAULT_PHOTO_PATH || HGT == DEFAULT_HGT || ISS == DEFAULT_ISS || EXP == DEFAULT_EXP || Class_License == DEFAULT_CLASS_LICENSE) return false;
+            if (NameSurname == DEFAULT_NAME_SURNAME || Number == DEFAULT_NUMBER ||
+                Adress == DEFAULT_ADRESS || DOB == DEFAULT_DOB ||
+                PhotoPath == DEFAULT_PHOTO_PATH || HGT == DEFAULT_HGT || ISS == DEFAULT_ISS ||
+                EXP == DEFAULT_EXP || Class_License == DEFAULT_CLASS_LICENSE) return false;
             else return true;
             // Данный метод используется для отслеживания заполенения профиля.
+        }
+
+        /// <summary>
+        /// Создает экзмепляр класса Driver c случайными данными
+        /// </summary>
+        /// <returns>Driver obj</returns>
+        public static Driver GetNewRandomDriver()
+        {
+            Random rd = new Random(); 
+
+            List<List<string>> names = new List<List<string>>(); // Список списков с именами.
+            names.Add(new List<string>() { "Alex", "Max", "Jhon" }); //Мужские имена
+            names.Add(new List<string>() { "Alice", "Ann", "Liz" }); // Женские имена
+            // Фамилии
+            List<string> surnames = new() {"Merser", "Smith", "Falei", "Asher", "Spilberg" };
+            List<string> adresses = new() { "Misnk", "London", "Paris", "San Jose", "Amsterdam" };
+            // Список доступных фото.Что бы фото доступно для выбора, вторая буква в имени должна 
+            // быть m или f. m- на фото мужчина , f-на фото женщина.
+            List<string> allPhotos = Directory.GetFiles("photo").ToList<string>();
+
+            // Случайное число для индекса выбора мужских или женских имен и фото. 0- мужские имена и фото
+            //1- Женские имена и фото
+            int gender_int = rd.Next(0, 2);
+            // Выбор имени согласно случайному индексу
+            string name = names[gender_int][rd.Next(names[gender_int].Count)];
+            string surname = surnames[rd.Next(surnames.Count)]; // Выбор фамилии
+
+            string r_nameAndSurname = name + " " + surname; // Имя и фамилия
+            string r_Adress = adresses[rd.Next(adresses.Count)];
+            DateTime r_Dob = new DateTime(rd.Next(1940, DateTime.Now.AddYears(-15).Year),
+                rd.Next(1, 13), rd.Next(1,29)); // Случайная дата рождения.Возраст не менее 16 лет
+            char r_Class_License = (char)('A' + rd.Next(6)); // Символ лицензии
+            int r_number = rd.Next(1, 10000000); // Номер лицензии
+            DateTime r_Iss = new DateTime(rd.Next(r_Dob.AddYears(17).Year, DateTime.Now.Year),
+                rd.Next(1,13), rd.Next(1,29)); // Случайная дата выдачи лицензии, с 16 лет от даты рождения
+            DateTime r_Exp = r_Iss.AddYears(rd.Next(1, 11)); // Случайная дата окончания.
+
+            bool r_Donor = rd.Next(1) == 0? true : false;
+            // Случайный выбор гендера и цвета глаз
+            GenderEnum r_Gender = (GenderEnum) rd.Next(Enum.GetValues(typeof(GenderEnum)).Length);
+            EyesEnum r_Eyes = (EyesEnum)rd.Next(Enum.GetValues(typeof(EyesEnum)).Length);
+            int r_Hgt = rd.Next(120, 211); // рост
+
+            //Список только мужчких или только женских фото
+            List<string> genderPhotos = new List<string>(); 
+
+            foreach(string path in allPhotos)
+            {
+                if (gender_int == 0) //Отбор всех фотографий только с мужчинами
+                {
+                    if (path[1] == 'm') genderPhotos.Add(path);       
+                }
+                ////Отбор всех фотографий только с женщинами
+                else if (gender_int == 1) { if (path[2] == 'f') genderPhotos.Add(path); } 
+            }
+            string r_PhotoPath = genderPhotos[ rd.Next(genderPhotos.Count)]; // Выбор случайного фото
+
+            // Экземпляр с набором случайных данных.
+            Driver newDriver = new Driver(r_nameAndSurname, r_number, r_Adress, r_Dob, r_Class_License,
+                r_Iss, r_Exp, r_Donor, r_PhotoPath, r_Gender, r_Eyes, r_Hgt);
+
+            return newDriver;
+            
         }
     }
 }
