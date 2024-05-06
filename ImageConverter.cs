1919df41c0c5de45f8b97de69c3391425aab8674
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2024-05-06 18:22:43 +0300

    Implemented a converter for selecting photo uploads and connected photo linking.

---
+++ ImageConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace SVPP_CS_WPF_Lab3_task1_Driving_license__Modification_
+{
+    class ImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new BitmapImage(new Uri(value.ToString(), UriKind.RelativeOrAbsolute));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
